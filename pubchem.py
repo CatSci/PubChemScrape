67d6c88d778daeca41755ecec37747f63ed5f522
CatSci <atul.yadav@catsci.com>
2022-09-02 13:35:21 +0100

    third commit

--- pubchem.py
+++ pubchem.py
@@ -17,48 +17,70 @@ cas_no = st.text_input('CAS Number')
 
 
 def get_name(data):
-    # name = driver.title
-    # name.split()
-    name = driver.find_element(By.CSS_SELECTOR, 'h1.m-zero')
-    compound_name = name.text
-    data['Name'] = compound_name
+    try:
+        name = driver.find_element(By.CSS_SELECTOR, 'h1.m-zero')
+        compound_name = name.text
+        data['Name'] = compound_name
+    except:
+        print("[INFO] Name is not avialable")
+        data['Name'] = 'None'
 
     return data
 
+
 def get_summary(data):
-    
-    temp_list = ['Molecular Formula', 'Molecular Weight']
-    summary = driver.find_element(By.CSS_SELECTOR, 'table.summary')
-    tr_tags = summary.find_elements(By.CSS_SELECTOR, 'tr')
+    try:
+        temp_list = ['Molecular Formula', 'Molecular Weight']
+        summary = driver.find_element(By.CSS_SELECTOR, 'table.summary')
+        tr_tags = summary.find_elements(By.CSS_SELECTOR, 'tr')
+        
+        if tr_tags:
+            for check_string in temp_list:
+                for tags in tr_tags:
+                    if check_string in tags.text:
+                        s = tags.text.split()
+                        data[check_string] = s[2]
 
-    for check_string in temp_list:
-        for tags in tr_tags:
-            if check_string in tags.text:
-                s = tags.text.split()
-                print(s)
-                data[check_string] = s[2]
-
-                break
+                        break
+    except:
+        print("[INFO] Molecule Formula and Molecule weight not found")
+        data['Molecular Formula'] = 'None'
+        data['Molecular Weight'] = 'None'
 
     return data
 
 
 def get_smile(data):
-    smile = driver.find_element(By.ID, 'Canonical-SMILES')
-    smile_code = smile.find_element(By.CSS_SELECTOR, 'p').text
-    data['Smile'] = smile_code
+    try:
+        smile = driver.find_element(By.ID, 'Canonical-SMILES')
+        smile_code = smile.find_element(By.CSS_SELECTOR, 'p').text
+        data['Smile'] = smile_code
+    except:
+        print('[INFO] Smile not found')
+        data['Smile'] = 'None'
 
     return data
 
-def get_ghs(hazard):
-    ghs = driver.find_element(By.ID, 'GHS-Classification')
-    ghs_string = ghs.find_elements(By.CSS_SELECTOR, 'p')
-    
-    for t in ghs_string:
-        r = re.findall(r'H[0-9][0-9][0-9].*', str(t.text))
+def get_h_statemenmt(info, hazard):
+    for j in info:
+        r = re.findall(r'H[0-9][0-9][0-9].*', str(j.text))
         if r:
             temp = r[0].split(':')
             hazard[temp[0]] = temp[1]
+    return hazard
+
+def get_ghs(hazard):
+    try:
+        ghs = driver.find_element(By.ID, 'GHS-Classification')
+        ghs_temp = ghs.find_elements(By.CSS_SELECTOR, 'div.breakword')
+        hazard = get_h_statemenmt(ghs_temp, hazard)
+
+
+        ghs_string = ghs.find_elements(By.CSS_SELECTOR, 'p')
+        hazard = get_h_statemenmt(ghs_string, hazard)
+    except:
+        print('[INFO] GHS not found')
+
     
     return hazard
 
@@ -94,11 +116,13 @@ def create_df_hazard(hazard):
 
 if st.button('Search'):
 
-
-    option = webdriver.ChromeOptions()
-    option.add_argument('--headless')
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(options = option, service= service)
+    try:
+        option = webdriver.ChromeOptions()
+        option.add_argument('--headless')
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(options = option, service= service)
+    except:
+        print("[INFO] Driver not laoded")
 
 
 
@@ -106,19 +130,19 @@ if st.button('Search'):
 
     driver.get(start_link)
     driver.implicitly_wait(5)
-    tmp = driver.find_elements(By.CSS_SELECTOR, 'span.breakword')
-#     st.write(tmp)
-    driver.implicitly_wait(4)
-    cid = tmp[1].text
-
-    n_link = start_link.split('#')[0]
-    link = n_link + 'compound/' + cid
+    try:
+        tmp = driver.find_elements(By.CSS_SELECTOR, 'span.breakword')
+        driver.implicitly_wait(4)
+        cid = tmp[1].text
+        n_link = start_link.split('#')[0]
+        link = n_link + 'compound/' + cid
+    except:
+        print("[INFO] Compound not avialable")
 
 
     driver.get(link)
     data, hazard = main()
     quit_driver()
-    st.write(hazard)
 
     df = create_df_data(data)
     st.write(df)    
